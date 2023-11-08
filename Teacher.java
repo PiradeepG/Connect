1a3fff28a3ddf98b455f9b1e2c4f62a9b4690344
sandy5310b <S.U.sarang2005@gmail.com>
2023-11-08 16:33:32 +0530

    fixed error

--- Teacher.java
+++ Teacher.java
@@ -1,28 +1,13 @@
-public class Teacher {
-    private String userName;
-    private String email;
-    private String password;
-    public void setUserName(String userName){
-        this.userName=userName;
+public class Teacher extends User
+{
+    Teacher teacher ;
+    String subject = "";
+    Teacher(String name , String password , String email , boolean isAdmin)
+    {
+        super(name, password, email, isAdmin);
     }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public String getUserName(){
-        return this.userName;
-    }
-    public String getEmail(){
-        return this.email;
-    }
-    public String getPassword(){
-        return this.password;
-    }
-    Teacher(String userName,String email,String password){
-        this.userName=userName;
-        this.email=email;
-        this.password=password;
+    public void start()
+    {
+            System.out.println("succesfully logined as a teacher");
     }
 }
